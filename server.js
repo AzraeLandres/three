bb644df8fb24876ebdf5159d486dd94ffcf1a170
AzraeLandres <azraellandres@gmail.com>
2024-03-14 12:50:50 +0100

    Update static file path in server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, ".")));
+const rootDir = path.join(__dirname, "07-cameras", "src");
+
+app.use(express.static(rootDir));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  res.sendFile(path.join(rootDir, "index.html"));
 });
 
 const port = process.env.PORT || 3000;
